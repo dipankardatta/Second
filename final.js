2521b56c2d31dbb88c60cb9e76f524c0ab594220
dipankardatta <dipankarzeus10@gmail.com>
2023-03-22 18:49:00 +0530

    using js to create attribute

--- final.js
+++ final.js
@@ -13,14 +13,124 @@
 //h2[0].style.fontWeight = 'bolder';
 //h2[0].style.color = 'green'
 
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[2].style.fontWeight = 'bold'
-li[2].style.backgroundColor = 'green'
-li[2].textContent = 'HELLO'
+//var li = document.getElementsByTagName('li');
+//console.log(li);
+//console.log(li[1]);
+//li[2].style.fontWeight = 'bold'
+//li[2].style.backgroundColor = 'green'
+//li[2].textContent = 'HELLO'
 
-for (var i = 0; i < li.length; i++){
-    li[i].style.backgroundColor = "#f4f4f4"
+//for (var i = 0; i < li.length; i++){
+    //li[i].style.backgroundColor = "#f4f4f4"
 
-}
+//}
+    //var header = document.querySelector('#main-header')
+    //header.style.borderBottom = 'solid 4px #ccc'
+
+    //var input = document.querySelector('input');
+    //input.value = "HELLO WORLD" // grab the first one by default
+
+    //var submit = document.querySelector('input[type="submit"]');
+    //submit.value = "SEND PLEASE"
+
+    //var item = document.querySelector('.list-group-item');
+    //item.style.color = 'red';
+
+    //var secondItem = document.querySelector('.list-group-item:nth-child(2)')
+    //secondItem.style.backgroundColor = 'green'
+
+    //var thirdItem = document.querySelector('.list-group-item:nth-child(3)')
+    //thirdItem.style.color = 'coral'
+
+//QUERYSELECTORALL
+
+
+//var odd = document.querySelectorAll('li:nth-child(odd)');
+//for (var i = 0; i<odd.length ; i++){
+  //  odd[i].style.backgroundColor = 'green'
+
+//}
+
+//TRAVERSING THE DOM
+var itemList = document.querySelector("#items");
+//PARENTNode
+//console.log(itemList.parentNode);
+//itemList.parentNode.style.backgroundColor = '#f6f6f6'
+//console.log(itemList.parentNode.parentNode)
+
+
+//PARENTELEMENT
+//console.log(itemList.parentElement);
+//itemList.parentElement.style.backgroundColor = '#f6f6f6'
+//console.log(itemList.parentElement.parentElement)
+
+
+//childNodes
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.background = 'yellow'
+
+//FirstChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'HELLO 1'
+
+//nextsibling
+// console.log(itemList.nextElementSibling)
+
+// previousSibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green'
+
+// create a div
+var newDiv = document.createElement('div');
+
+// add class
+newDiv.className = 'hello';
+
+// add id
+newDiv.id = 'hello1';
+
+// add attr
+newDiv.setAttribute('title','Hello Div');
+
+//create text node
+var newDivText = document.createTextNode("HELLO WORLD");
+
+// add text to div
+
+newDiv.appendChild(newDivText);
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+container.insertBefore(newDiv, h1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
